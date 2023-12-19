eb0eb08a6ce0eb70c751f6476c6cbb2874ddd381
bt.7274(김홍균)/kakao <bt.7274@kakaocorp.com>
2023-12-20 00:04:27 +0900

    sol: 11653

---
+++ 11653/11653_slow.py
@@ -0,0 +1,27 @@
+"""
+# BOJ
+# No. 11653
+# Python 3.10.4
+# by "nno0obb"
+"""
+
+
+def main():
+    # Input
+    N = int(input())
+
+    # Logic
+    f = 2
+    while N > 1:
+        if N % f == 0:
+            N /= f
+
+            # Output
+            print(f)
+
+        else:
+            f += 1
+
+
+if __name__ == '__main__':
+    main()
